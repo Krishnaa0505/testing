48a036a4fbd82174c72d04783277bd686bcdd346
Lakshay Menghani <lakshaymenghani@gmail.com>
2025-05-26 16:58:57 +0530

    A simple factorial function for testing

---
+++ factorial.py
@@ -0,0 +1,11 @@
+def fact(a):
+    n=1
+    for i in range(1,a+1):
+        n*=i
+    
+    print(f"The factorial of {a} is {n}")
+    #return n
+
+if __name__ == '__main__':
+    n=int(input("Enter the number to find factorial of"))
+    fact(n)
